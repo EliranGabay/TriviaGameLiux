111781e5e9c32d5fc2e1af1b046d12227d55ee79
Lior <liors1992@gmail.com>
2019-06-16 12:48:43 +0300

    push

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+game: 
+	chmod +x start.py 
+
+
+install:
+	sudo cp start.py game.py logo.py sql.py /usr/local/bin/
+
--- start.py
+++ start.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'start.ui'
-#
-# Created by: PyQt4 UI code generator 4.12.1
-#
-# WARNING! All changes made in this file will be lost!
 import sys
 from PyQt4 import QtCore, QtGui
 import game
